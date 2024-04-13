a41cafef12dd701f3b3256d2d24a29a032babfc1
Balbisaur <albi3479@yahoo.com>
2024-04-13 00:36:47 -0500

    first commit

---
+++ The_Adventure_Game.py
@@ -0,0 +1,8 @@
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    print('climb a tree!')
+elif place == "cave":
+        print("You found a hidden treasure!")
+else:
+    print("cross a river")
